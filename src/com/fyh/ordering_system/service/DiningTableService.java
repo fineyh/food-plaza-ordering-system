5c28b01f11584c1057c0622be3985adc174bc6bf
fineyh <746485169@qq.com>
2022-12-04 23:19:15 +0800

    完成预定餐桌功能
    完成预定餐桌功能。

--- src/com/fyh/ordering_system/service/DiningTableService.java
+++ src/com/fyh/ordering_system/service/DiningTableService.java
@@ -13,4 +13,17 @@ public class DiningTableService { // 业务层
     public List<DiningTable> list() {
         return diningTableDAO.queryMulti("select id, state from diningTable", DiningTable.class);
     }
+
+    //根据id，查询对应的餐桌DiningTable 对象
+    //如果返回null，表示id编号对应的餐桌不存在
+    public DiningTable getDiningTableById(int id) {
+        return  diningTableDAO.querySingle("select * from diningTable where id = ?", DiningTable.class, id);
+    }
+
+    //如果餐桌可以预定，调用方法，对其状态进行更新（包括预定人的名字和电话）
+    public boolean orderDiningTable(int id, String orderName, String orderTel) {
+        //返回一个整数，大于0成功，不大于0，失效
+        int update = diningTableDAO.update("update diningTable set state='Ordered', orderName=?, orderTel=? where id=?", orderName, orderTel, id);
+        return update > 0;
+    }
 }
--- src/com/fyh/ordering_system/view/View.java
+++ src/com/fyh/ordering_system/view/View.java
@@ -23,6 +23,48 @@ public class View {
         System.out.println("=========Display Complete=========");
     }
 
+    //完成订座
+    public void orderDiningTable() {
+        System.out.println("=========Order Dining Table=========");
+        System.out.println("Please select the table number (-1 to exit)");
+        int orderId = Utility.readInt();
+        if(orderId == -1) {
+            System.out.println("=========Cancel the table reservation=========");
+            return;
+        }
+        //该方法得到的是 Y 或者 N
+        char key = Utility.readConfirmSelection();
+        if (key == 'Y') { // 要预定
+            //根据 orderId 返回对应DiningTable对象，如果为null，说明该对象不存在
+            DiningTable diningTable = diningTableService.getDiningTableById(orderId);
+            if (diningTable == null) { // 输入餐桌不存在
+                System.out.println("=========The reserved table does not exist=========");
+                return;
+            }
+            //判断该餐桌的状态是否为“empty”
+            if (!("empty".equals(diningTable.getState()))) { // 说明当前这张餐桌不是“empty”状态
+                System.out.println("=========The table has been reserved or is being served=========");
+                return;
+            }
+
+            //接收预定信息
+            System.out.print("Name of the reservation person: ");
+            String orderName = Utility.readString(50);
+            System.out.print("Phone number of the reservation person: ");
+            String orderTel = Utility.readString(50);
+
+            //更新餐桌状态
+            if (diningTableService.orderDiningTable(orderId, orderName, orderTel)) {
+                System.out.println("=========Order the table successfully=========");
+            } else {
+                System.out.println("=========Order the table failed=========");
+            }
+
+        } else {
+            System.out.println("=========Cancel the table reservation=========");
+        }
+    }
+
     //控制是否退出菜单
     private boolean loop = true;
     private String key = ""; // 接收用户的选择
@@ -42,9 +84,9 @@ public class View {
 
             switch (key) {
                 case "1":
-                    System.out.println("Account Number: ");
+                    System.out.print("Account Number: ");
                     String empId = Utility.readString(50);
-                    System.out.println("Password: ");
+                    System.out.print("Password: ");
                     String pwd = Utility.readString(50);
                     //到数据库去判断
                     Employee employee = employeeService.getEmployeeByIdAndPwd(empId,pwd);
@@ -54,20 +96,20 @@ public class View {
                         while (loop) {
                             System.out.println("=========Food Plaza Ordering System(Secondary menu)=========");
                             System.out.println("\t\t 1 Show table status");
-                            System.out.println("\t\t 2 Book a table");
+                            System.out.println("\t\t 2 Order a table");
                             System.out.println("\t\t 3 Show all dishes");
                             System.out.println("\t\t 4 Order service");
                             System.out.println("\t\t 5 View bill");
                             System.out.println("\t\t 6 Settle accounts");
                             System.out.println("\t\t 7 Exit");
-                            System.out.println("Please enter your choice: ");
+                            System.out.print("Please enter your choice: ");
                             key = Utility.readString(1);
                             switch (key) {
                                 case "1":
                                     listDiningTable(); //显示餐桌状态
                                     break;
                                 case "2":
-                                    System.out.println("Book a table");
+                                    orderDiningTable(); //预定餐桌
                                     break;
                                 case "3":
                                     System.out.println("Show all dishes");
